63d42d0d5c535060f3e12799b6739b8980ee2f4c
AlexOfili <ofilia@stpaulsschool.org.uk>
2026-07-24 13:00:23 +0100

    products list added to data

---
+++ data/products.js
@@ -0,0 +1,8 @@
+const PRODUCTS = [
+  { id: 1, name: "Oat Milk 1L", price: 1.35, aisle: "Dairy Alt" },
+  { id: 2, name: "Sourdough Loaf", price: 2.20, aisle: "Bakery" },
+  { id: 3, name: "Cherry Tomatoes 300g", price: 1.80, aisle: "Produce" },
+  { id: 4, name: "Free Range Eggs (6)", price: 2.10, aisle: "Dairy" },
+  { id: 5, name: "Cheddar Cheese 200g", price: 2.75, aisle: "Dairy" },
+  { id: 6, name: "Basmati Rice 1kg", price: 2.40, aisle: "Grocery" },
+];
